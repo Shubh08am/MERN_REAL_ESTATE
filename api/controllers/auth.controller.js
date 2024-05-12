078446dbf7ae97ad6e431a95275dfcfbf5b2eaf2
Shubham Yadav <shubh08am@gmail.com>
2024-05-12 15:56:18 +0530

    create and add OAuth component

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,3 +46,42 @@ export const signin = async (req,res,next)=>{
     next(error);
  }
 };
+
+export const google = async (req, res, next) => {
+   try {
+      //check if user existed or not -> if not create user -> else signin the user
+     const user = await User.findOne({ email: req.body.email });
+     //user exixts and authenticating them and signing them
+     if (user) {
+       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+       const { password: pass, ...rest } = user._doc;
+       res
+         .cookie('access_token', token, { httpOnly: true })
+         .status(200)
+         .json(rest); //sending back the user data 
+     } else { 
+      //create the user , in models -> user.model.js -> requires password: true -> create your own password
+       const generatedPassword =
+         Math.random().toString(36).slice(-8) +
+         Math.random().toString(36).slice(-8);
+       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+       const newUser = new User({
+         username:
+           req.body.name.split(' ').join('').toLowerCase() +
+           Math.random().toString(36).slice(-4),
+         email: req.body.email,
+         password: hashedPassword,
+         avatar: req.body.photo,
+       });
+       await newUser.save();
+       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+       const { password: pass, ...rest } = newUser._doc;
+       res
+         .cookie('access_token', token, { httpOnly: true })
+         .status(200)
+         .json(rest);
+     }
+   } catch (error) {
+     next(error);
+   }
+ };
