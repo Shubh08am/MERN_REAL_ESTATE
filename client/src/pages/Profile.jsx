a82d1f754633a9aea458c1e061c39d446cc57031
Shubham Yadav <shubh08am@gmail.com>
2024-05-13 20:28:53 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,7 +5,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/s
 import { app } from '../firebase';
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-
+import {Link} from 'react-router-dom';
 
 
 export default function Profile() {
@@ -172,6 +172,10 @@ const handleSignOut = async () => {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
 
       </form>
 
