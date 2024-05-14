4b84d40e8145a31e08110587e9e85946820eb1b4
Shubham Yadav <shubh08am@gmail.com>
2024-05-15 00:11:36 +0530

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,6 @@
 
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -10,3 +11,21 @@ export const createListing = async (req, res, next) => {
   }
 };
 
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  // check if the listing exist or not if not return error 
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+  //if listing exist check if the user is the owner of listing 
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only delete your own listings!'));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json('Listing has been deleted!');
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -133,6 +133,25 @@ const handleShowListings = async () => {
   }
 };
 
+const handleListingDelete = async (listingId) => {
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`, {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if (data.success === false) {
+      console.log(data.message);
+      return;
+    }
+    //from the previous state we want all data except the current list to be deleted -> delete it & update listings 
+    setUserListings((prev) =>
+      prev.filter((listing) => listing._id !== listingId)
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -241,7 +260,7 @@ const handleShowListings = async () => {
               </Link>
   
               <div className='flex flex-col item-center'>
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button  onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
